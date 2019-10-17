50e8159cf1bae96a3a663e56436547879e683699
egenomics <joseluis.villanueva@crg.eu>
2019-10-17 10:37:10 +0200

    crop to 20

--- chipseq-16.04/chipseq.sh
+++ chipseq-16.04/chipseq.sh
@@ -304,7 +304,8 @@ trim_reads_trimmomatic() {
 					LEADING:$leading \
 					TRAILING:$trailing \
 					MAXINFO:$targetLength:$strictness \
-					MINLEN:$minLength \ >$step_log 2>&1
+					MINLEN:$minLength \
+					CROP:20 >$step_log 2>&1
 
 	# parse step log to extract generated metadata
 	message_info $step "parse step log to extract generated metadata"
